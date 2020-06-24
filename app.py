138f9146e3b7bcec14b89746541126a02e827737
jenslaufer <jenslaufer@gmail.com>
2020-06-24 15:38:42 +0200

    added host to get eve app accessible in the docker container

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import os
 from eve import Eve
-import logging
-
-loglevel = int(os.environ.get('LOGLEVEL', 10))
-logging.basicConfig(level=loglevel)
 
 app = Eve()
-host = os.environ.get('HOST', '0.0.0.0')
-
-if __name__ == "__main__":
-    app.run(host=host)
+app.run(host="0.0.0.0")
